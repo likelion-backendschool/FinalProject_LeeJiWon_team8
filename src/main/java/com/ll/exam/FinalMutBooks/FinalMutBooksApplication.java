152693841e14d4f36422bcbb5e6828df1d1e68e5
jiooong <lucy991010@gmail.com>
2022-10-18 14:50:01 +0900

    feat: setting

---
+++ src/main/java/com/ll/exam/FinalMutBooks/FinalMutBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.FinalMutBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalMutBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalMutBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/FinalMutBooks/FinalMutBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.FinalMutBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalMutBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
